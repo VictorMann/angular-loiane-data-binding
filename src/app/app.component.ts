dd1b34d820ae96fac9ff2f1271c2237b8672ae7e
Victor Mann <victorcmann@gmail.com>
2019-05-06 22:26:01 -0300

    ciclo de vida do componente

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  valorCiclo: number = 5;
+  isDestroiValor: boolean = false;
+
+  mudarValorCiclo() {
+    this.valorCiclo ++;
+  }
+
+  onDestruir() {
+    this.isDestroiValor = true;
+  }
 }
